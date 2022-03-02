baef4db92e985cbeb2d9c3ac9f804c0dffc37396
Jhonatan Keretz <92455620+JhonatanKeretz@users.noreply.github.com>
2022-03-02 09:14:08 -0300

    Add files via upload

--- src/components/header_logged/index.js
+++ src/components/header_logged/index.js
@@ -2,7 +2,7 @@ import React, { Fragment, useState } from 'react';
 import { Navbar, Container, Column, Button, Dropdown } from 'rbx';
 import LogoImage from '../../assets/images/logo-white.png';
 import "../../styles/header.scss";
-import UserService from '../../services/users';
+import UserService from '../../services/user';
 import { Route, Routes, Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faList } from '@fortawesome/free-solid-svg-icons'
